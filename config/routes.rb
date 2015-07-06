b978a644128736578e36f97c084c1b99965a4190
Michael E. Nash <utumno86@gmail.com>
2015-07-06 14:16:43 -0400

    installed google omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
-  devise_for :users
+  devise_for :users, :controllers =>{
+                       :registrations => 'registrations',
+                       :omniauth_callbacks => "users/omniauth_callbacks"
+                   }
+
   resources :users
 end
