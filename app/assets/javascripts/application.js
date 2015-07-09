10d630a2b8ff14df79de649ee78df1e73fdbdf35
Michael E. Nash <utumno86@gmail.com>
2015-07-09 18:12:14 -0400

    more complete homepage

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,9 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).ready(function() {
+    $('.carousel').carousel({
+        interval: 2000 //changes the speed
+    })
+});
